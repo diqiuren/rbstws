4fd8f5a60f6b589ba9b0b76cd4106c80d3f03670
americo.dias <americo.dias@a1099118-2dca-11df-bfeb-7b4c664f286a>
2011-04-12 14:31:32 +0000

    Safecheck almost done...

--- server/src/safecheck.h
+++ server/src/safecheck.h
@@ -17,4 +17,17 @@
 #ifndef __SAFECHECK_H
 #define __SAFECHECK_H
 
+#define __SAFECHECK_TB		1
+
+typedef struct {
+    unsigned int nbits;
+    char *tdo;
+    char *mask;
+    struct safecheck_t *next_safecheck;
+} safecheck_t;
+
+void safecheck_clear(unsigned int tap);
+int safecheck_add(unsigned int tap, safecheck_t *safechk);
+unsigned int safecheck(void);
+
 #endif //__SAFECHECK_H
